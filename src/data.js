5187d9a4a3da8da99ac0725e0b038438fa79a375
moenga60 <moengadaniel60@gmail.com>
2024-02-06 14:59:17 +0300

    first comit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Social Media App",
+        img: "assets/man.jpeg",
+    },
+    {
+        id: 2,
+        title: "UI",
+        img: "assets/man.jpeg",
+    },
+    {
+        id: 3,
+        title: "E-commerce",
+        img: "assets/man.jpeg",
+    },
+    {
+        id: 4,
+        title: "relax",
+        img: "assets/man.jpeg",
+    },
+    {
+        id: 5,
+        title: "mnsjjhd",
+        img: "assets/man.jpeg",
+    },
+    {
+        id: 6,
+        title: "gshsgsgs",
+        img: "assets/man.jpeg",
+    }
+]
